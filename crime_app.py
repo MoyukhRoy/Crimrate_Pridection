8089dbc006dd11126ab75717bfdc5a2ca4e89dea
Moyukh <moyukhroyniitsbz@gmail.com>
2025-03-13 18:59:57 +0530

    Create crime_app.py

---
+++ crime_app.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import streamlit as st
+
+
+
+
+@st.cache_data 
+def load_data():
+    data = pd.read_csv(
+        '/Users/user/Desktop/crime_rate/crime.csv')
+    return data
+
+
+data = load_data()
+# Create a Streamlit web app
+st.title('Crime Data Filter')
+
+# Create input widgets for filtering
+crime_type = st.sidebar.selectbox('Select Crime Type:', data['TYPE'].unique())
+neighborhood = st.sidebar.selectbox('Select Neighborhood:', data['NEIGHBOURHOOD'].unique())
+year = st.sidebar.selectbox('Select Year:', data['YEAR'].unique())
+
+# Filter the data based on user input
+filtered_data = data[
+    (data['TYPE'] == crime_type) &
+    (data['NEIGHBOURHOOD'] == neighborhood) &
+    (data['YEAR'] == year)
+    ]
+
+# Display the filtered data
+st.write(f"Displaying {len(filtered_data)} records")
+st.write(filtered_data)
+
